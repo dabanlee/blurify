b8ec2eb5cfb906b9e0d332c5f0ac6b6eed0a499f
Just <576839360@qq.com>
2017-03-02 01:57:43 +0800

    📦 add webpack builder

---
+++ webpack.config.babel.js
@@ -0,0 +1,34 @@
+import path from 'path';
+
+let configure = {};
+
+configure = {
+    entry: {
+        'blurify': [
+            './src/index.js',
+        ],
+    },
+    output: {
+        path: '../dist',
+        // publicPath:'', // public path
+        filename: 'js/[name].js?[hash:8]', // output file name
+    },
+    module: {
+        loaders: [{
+            test: /\.(js|es6)$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+        }, {
+            test: /\.(woff|ttf|eot|svg|gif|jpg|png)$/,
+            loader: 'url-loader?limit=5120&name=[path][name].[ext]?[hash:8]', // Images which less than 5K turn into base64.
+        }, {
+            test: /\.json$/,
+            loader: 'json-loader',
+        }],
+    },
+    resolve: {
+        extensions: ['.js', '.es6', '.scss', '.css'],
+    },
+};
+
+module.exports = configure;
