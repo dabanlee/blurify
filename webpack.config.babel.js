27425b304764612b8eeb2bb99baaa752f59f282b
Just <576839360@qq.com>
2017-03-03 17:07:23 +0800

    ✨ update webpack configure

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,23 +3,19 @@ export default {
         'blurify': [
             './src/index.js',
         ],
+        'blurify.min': [
+            './src/index.js',
+        ],
     },
     output: {
         path: '../dist',
-        // publicPath:'', // public path
-        filename: 'js/[name].js?[hash:8]', // output file name
+        filename: 'js/[name].js?[hash:8]',
     },
     module: {
         loaders: [{
             test: /\.(js|es6)$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
-        }, {
-            test: /\.(woff|ttf|eot|svg|gif|jpg|png)$/,
-            loader: 'url-loader?limit=5120&name=[path][name].[ext]?[hash:8]', // Images which less than 5K turn into base64.
-        }, {
-            test: /\.json$/,
-            loader: 'json-loader',
         }],
     },
     resolve: {
